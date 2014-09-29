a14c00836d7e3ff2b9d1480e9e4e8ea7d010f9b0
Huang Yaming <yumminhuang@gmail.com>
2014-09-28 22:17:09 -0400

    Change process_execute() in process.c
    1. Modify README
    2. Add DESIGNDOC for Project 2

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,14 +7,28 @@
 static void syscall_handler (struct intr_frame *);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
 }
+
+void halt (void);
+void exit (int status);
+pid_t exec (const char *cmd_line);
+int wait (pid_t pid);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
